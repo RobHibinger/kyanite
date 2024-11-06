1bc1aba00e585476c815d9e7286b88addc8070ef
Rob Hibinger <robhibinger@gmail.com>
2024-11-06 17:55:16 -0500

    Started adding animation state

--- main.go
+++ main.go
@@ -5,14 +5,42 @@ import (
 	"image"
 	"image/color"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Camera2D struct {
-	w, h, x, y float64
+type SpriteResouceType int
+
+const (
+	SpriteResouceType_KnightIdle SpriteResouceType = iota
+	SpriteResouceType_KnightWalkNorth
+	SpriteResouceType_KnightWalkEast
+	SpriteResouceType_KnightWalkSouth
+	SpriteResouceType_KnightWalkWest
+)
+
+type SpriteResource struct {
+	Width, Height int
+	Image         *ebiten.Image
+	Frames        []Vec2
+}
+
+type SpriteState struct {
+	Type  SpriteResouceType
+	Index int
+}
+
+type SpriteAnimState struct {
+	SpriteState
+	StartTime time.Time
+	Duration  time.Duration
+}
+
+func (sas *SpriteAnimState) Update() {
+
 }
 
 type Vec2 struct {
@@ -20,68 +48,94 @@ type Vec2 struct {
 }
 
 type Entity struct {
-	position Vec2
-	scale    Vec2
-	image    *ebiten.Image
+	Position        Vec2
+	Scale           Vec2
+	Velocity        Vec2
+	Sprite          SpriteState
+	SpriteAnimState SpriteAnimState
 }
 
-func (e *Entity) Draw(screen *ebiten.Image) {
+func (e *Entity) Draw(screen *ebiten.Image, game *Game) {
 	opts := ebiten.DrawImageOptions{}
-	opts.GeoM.Scale(e.scale.x, e.scale.y)
-	opts.GeoM.Translate(e.position.x, e.position.y)
+	opts.GeoM.Scale(e.Scale.x, e.Scale.y)
+	opts.GeoM.Translate(e.Position.x, e.Position.y)
+
+	sprite := game.AnimSpriteResources[e.Sprite.Type]
+	currentCell := sprite.Frames[e.Sprite.Index]
 	screen.DrawImage(
-		e.image.SubImage(
-			image.Rect(0, 0, 16, 16),
+		sprite.Image.SubImage(
+			image.Rect(int(currentCell.x)*sprite.Width, int(currentCell.y)*sprite.Height, int(currentCell.x)*sprite.Width+sprite.Width, int(currentCell.y)*sprite.Height+sprite.Height),
 		).(*ebiten.Image),
 		&opts,
 	)
 }
 
+type InputState struct {
+	MoveDirection Vec2
+}
+
 type GameState struct {
-	camera   Camera2D
-	player   Entity
-	entities []Entity
+	Player   Entity
+	Entities []Entity
 }
 
 type Game struct {
-	debug      bool
-	game_state GameState
+	Debug               bool
+	GameState           GameState
+	InputState          InputState
+	AnimSpriteResources map[SpriteResouceType]SpriteResource
 }
 
 func (g *Game) HandleInput() {
 	if ebiten.IsKeyPressed(ebiten.KeyF1) && inpututil.IsKeyJustPressed(ebiten.KeyF1) {
-		g.debug = !g.debug
+		g.Debug = !g.Debug
 	}
 
+	g.InputState.MoveDirection = Vec2{}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.game_state.player.position.y += -1
+		g.InputState.MoveDirection.y = -1
 	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.game_state.player.position.y += 1
+		g.InputState.MoveDirection.y = 1
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.game_state.player.position.x -= 1
+		g.InputState.MoveDirection.x = -1
 	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.game_state.player.position.x += 1
+		g.InputState.MoveDirection.x = 1
 	}
 }
 
 func (g *Game) Update() error {
 	g.HandleInput()
+
+	g.GameState.Player.Velocity = g.InputState.MoveDirection
+	g.GameState.Player.Position.x += g.GameState.Player.Velocity.x
+	g.GameState.Player.Position.y += g.GameState.Player.Velocity.y
+
+	if g.InputState.MoveDirection.y < 0 {
+		// set player animation state to walk north
+	} else if g.InputState.MoveDirection.y > 0 {
+		// set player animation state to walk south
+	} else if g.InputState.MoveDirection.x < 0 {
+		// set player animation state to walk west
+	} else if g.InputState.MoveDirection.x > 0 {
+		// set player animation state to walk east
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{36, 54, 66, 0})
 
-	if g.debug {
+	if g.Debug {
 		ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %v", ebiten.ActualFPS()))
 	}
 
-	g.game_state.player.Draw(screen)
+	g.GameState.Player.Draw(screen, g)
 
-	for _, e := range g.game_state.entities {
-		e.Draw(screen)
+	for _, e := range g.GameState.Entities {
+		e.Draw(screen, g)
 	}
 }
 
@@ -94,21 +148,83 @@ func main() {
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	playerImg, _, err := ebitenutil.NewImageFromFile("assets/images/Knight/SpriteSheet.png")
+	knightIdle, _, err := ebitenutil.NewImageFromFile("assets/images/Knight/SeparateAnim/Idle.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	KnightWalk, _, err := ebitenutil.NewImageFromFile("assets/images/Knight/SeparateAnim/Walk.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var g = Game{
-		debug: false,
-		game_state: GameState{
-			player: Entity{
-				image: playerImg,
-				position: Vec2{
+		Debug: false,
+		AnimSpriteResources: map[SpriteResouceType]SpriteResource{
+			SpriteResouceType_KnightIdle: {
+				Image:  knightIdle,
+				Height: 16,
+				Width:  16,
+				Frames: []Vec2{
+					{x: 0, y: 0},
+				},
+			},
+			SpriteResouceType_KnightWalkNorth: {
+				Image:  KnightWalk,
+				Height: 16,
+				Width:  16,
+				Frames: []Vec2{
+					{x: 1, y: 0},
+					{x: 1, y: 1},
+					{x: 1, y: 2},
+					{x: 1, y: 3},
+				},
+			},
+			SpriteResouceType_KnightWalkEast: {
+				Image:  KnightWalk,
+				Height: 16,
+				Width:  16,
+				Frames: []Vec2{
+					{x: 3, y: 0},
+					{x: 3, y: 1},
+					{x: 3, y: 2},
+					{x: 3, y: 3},
+				},
+			},
+			SpriteResouceType_KnightWalkSouth: {
+				Image:  KnightWalk,
+				Height: 16,
+				Width:  16,
+				Frames: []Vec2{
+					{x: 0, y: 0},
+					{x: 0, y: 1},
+					{x: 0, y: 2},
+					{x: 0, y: 3},
+				},
+			},
+			SpriteResouceType_KnightWalkWest: {
+				Image:  KnightWalk,
+				Height: 16,
+				Width:  16,
+				Frames: []Vec2{
+					{x: 2, y: 0},
+					{x: 2, y: 1},
+					{x: 2, y: 2},
+					{x: 2, y: 3},
+				},
+			},
+		},
+		GameState: GameState{
+			Player: Entity{
+				Sprite: SpriteState{
+					Type:  SpriteResouceType_KnightIdle,
+					Index: 0,
+				},
+				Position: Vec2{
 					x: 100.0,
 					y: 100.0,
 				},
-				scale: Vec2{
+				Scale: Vec2{
 					x: 2.0,
 					y: 2.0,
 				},
